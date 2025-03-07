574ecd290ecd0f86a74fffc421d1d1e5602ca86b
diephauthan <diephauthan@gmail.com>
2025-03-07 22:23:09 +0700

    update

--- 002pseudo_char_driver/main.mod
+++ 002pseudo_char_driver/main.mod
@@ -1,2 +1,1 @@
 /home/than-diep/workspace/ldd/custom_drivers/002pseudo_char_driver/main.o
-
--- 002pseudo_char_driver/main.mod.c
+++ 002pseudo_char_driver/main.mod.c
@@ -1,10 +1,18 @@
 #include <linux/module.h>
 #define INCLUDE_VERMAGIC
 #include <linux/build-salt.h>
+#include <linux/elfnote-lto.h>
+#include <linux/export-internal.h>
 #include <linux/vermagic.h>
 #include <linux/compiler.h>
 
+#ifdef CONFIG_UNWINDER_ORC
+#include <asm/orc_header.h>
+ORC_HEADER;
+#endif
+
 BUILD_SALT;
+BUILD_LTO_INFO;
 
 MODULE_INFO(vermagic, VERMAGIC_STRING);
 MODULE_INFO(name, KBUILD_MODNAME);
@@ -19,18 +27,51 @@ __section(".gnu.linkonce.this_module") = {
 	.arch = MODULE_ARCH_INIT,
 };
 
-#ifdef CONFIG_RETPOLINE
+#ifdef CONFIG_MITIGATION_RETPOLINE
 MODULE_INFO(retpoline, "Y");
 #endif
 
-static const struct modversion_info ____versions[]
-__used __section("__versions") = {
-	{ 0x7f2455b3, "module_layout" },
-	{ 0xa6a08f14, "cdev_add" },
-	{ 0x2a08ad43, "cdev_init" },
-	{ 0xe3ec2f2b, "alloc_chrdev_region" },
-	{ 0xefd6cf06, "__aeabi_unwind_cpp_pr0" },
-};
+
+
+static const char ____versions[]
+__used __section("__versions") =
+	"\x1c\x00\x00\x00\x2b\x2f\xec\xe3"
+	"alloc_chrdev_region\0"
+	"\x14\x00\x00\x00\x1e\x70\xe8\x75"
+	"cdev_init\0\0\0"
+	"\x14\x00\x00\x00\x5e\xc3\x64\xaa"
+	"cdev_add\0\0\0\0"
+	"\x18\x00\x00\x00\x6b\xaa\x0f\xe7"
+	"class_create\0\0\0\0"
+	"\x18\x00\x00\x00\xfe\xc8\xfb\x9d"
+	"device_create\0\0\0"
+	"\x14\x00\x00\x00\x69\x1b\x25\x12"
+	"cdev_del\0\0\0\0"
+	"\x24\x00\x00\x00\x33\xb3\x91\x60"
+	"unregister_chrdev_region\0\0\0\0"
+	"\x18\x00\x00\x00\x7f\xe7\x31\xb2"
+	"class_destroy\0\0\0"
+	"\x18\x00\x00\x00\x4b\xc2\x7c\x8c"
+	"device_destroy\0\0"
+	"\x18\x00\x00\x00\xc2\x9c\xc4\x13"
+	"_copy_from_user\0"
+	"\x14\x00\x00\x00\xbb\x6d\xfb\xbd"
+	"__fentry__\0\0"
+	"\x10\x00\x00\x00\x7e\x3a\x2c\x12"
+	"_printk\0"
+	"\x1c\x00\x00\x00\xca\x39\x82\x5b"
+	"__x86_return_thunk\0\0"
+	"\x1c\x00\x00\x00\x48\x9f\xdb\x88"
+	"__check_object_size\0"
+	"\x18\x00\x00\x00\xe1\xbe\x10\x6b"
+	"_copy_to_user\0\0\0"
+	"\x28\x00\x00\x00\xb3\x1c\xa2\x87"
+	"__ubsan_handle_out_of_bounds\0\0\0\0"
+	"\x18\x00\x00\x00\xde\x9f\x8a\x25"
+	"module_layout\0\0\0"
+	"\x00\x00\x00\x00\x00\x00\x00\x00";
 
 MODULE_INFO(depends, "");
 
+
+MODULE_INFO(srcversion, "CB88D4710F31B8BBD87A740");
